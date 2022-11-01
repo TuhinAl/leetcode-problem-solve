5825eb87f6abe23c0f5e220491be0a06a593c7b9
tuhin <alauddintuhin1411@gmail.com>
2022-11-01 22:37:40 +0600

    doubly linkedlist

---
+++ src/com/altuhin/datastructure/Info.java
@@ -0,0 +1,7 @@
+package com.altuhin.datastructure;
+
+public class Info {
+public static void main(String[] args) {
+	System.out.println("tuhinal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Tuhinal");
+}
+}
